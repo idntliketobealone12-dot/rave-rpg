c059b84c6d26c140caed32b2bb5868c7861c53b8
idntliketobealone12-dot <idntliketobealone12@gmail.com>
2026-01-25 03:52:57 +0900

    Add files via upload

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const RAVE = {};
+
+/**
+ * The set of Ability Scores used within the system.
+ * @type {Object}
+ */
+RAVE.abilities = {
+  str: 'RAVE.Ability.Str.long',
+  dex: 'RAVE.Ability.Dex.long',
+  con: 'RAVE.Ability.Con.long',
+  int: 'RAVE.Ability.Int.long',
+  wis: 'RAVE.Ability.Wis.long',
+  cha: 'RAVE.Ability.Cha.long',
+};
+
+RAVE.abilityAbbreviations = {
+  str: 'RAVE.Ability.Str.abbr',
+  dex: 'RAVE.Ability.Dex.abbr',
+  con: 'RAVE.Ability.Con.abbr',
+  int: 'RAVE.Ability.Int.abbr',
+  wis: 'RAVE.Ability.Wis.abbr',
+  cha: 'RAVE.Ability.Cha.abbr',
+};
